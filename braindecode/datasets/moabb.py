77f1c29d0e36cb83f5b6057039c3eac60f6a28e1
shiva <shiva_roshanravan@yahoo.com>
2021-05-31 03:07:55 +0430

    Add path to moabb class

--- braindecode/datasets/moabb.py
+++ braindecode/datasets/moabb.py
@@ -24,7 +24,8 @@ def _find_dataset_in_moabb(dataset_name):
     raise ValueError("'dataset_name' not found in moabb datasets")
 
 
-def _fetch_and_unpack_moabb_data(dataset, subject_ids):
+def _fetch_and_unpack_moabb_data(dataset, subject_ids, path=None):
+    dataset.data_path(subject=subject_ids[0], path=path)
     data = dataset.get_data(subject_ids)
     raws, subject_ids, session_ids, run_ids = [], [], [], []
     for subj_id, subj_data in data.items():
@@ -61,7 +62,7 @@ def _annotations_from_moabb_stim_channel(raw, dataset):
     return annots
 
 
-def fetch_data_with_moabb(dataset_name, subject_ids):
+def fetch_data_with_moabb(dataset_name, subject_ids, path):
     # ToDo: update path to where moabb downloads / looks for the data
     """Fetch data using moabb.
 
@@ -79,7 +80,7 @@ def fetch_data_with_moabb(dataset_name, subject_ids):
     """
     dataset = _find_dataset_in_moabb(dataset_name)
     subject_id = [subject_ids] if isinstance(subject_ids, int) else subject_ids
-    return _fetch_and_unpack_moabb_data(dataset, subject_id)
+    return _fetch_and_unpack_moabb_data(dataset, subject_id, path)
 
 
 class MOABBDataset(BaseConcatDataset):
@@ -91,8 +92,8 @@ class MOABBDataset(BaseConcatDataset):
     subject_ids: list(int) | int
         (list of) int of subject(s) to be fetched
     """
-    def __init__(self, dataset_name, subject_ids):
-        raws, description = fetch_data_with_moabb(dataset_name, subject_ids)
+    def __init__(self, dataset_name, subject_ids, path):
+        raws, description = fetch_data_with_moabb(dataset_name, subject_ids, path)
         all_base_ds = [BaseDataset(raw, row)
                        for raw, (_, row) in zip(raws, description.iterrows())]
         super().__init__(all_base_ds)
